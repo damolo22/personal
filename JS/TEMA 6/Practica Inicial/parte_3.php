48cad652ac76d346e51876219d37cd426b8d2510
damolo22 <damolo2006@gmail.com>
2026-01-21 17:07:03 +0100

    hola

--- JS/TEMA 6/Practica Inicial/parte_3.php
+++ JS/TEMA 6/Practica Inicial/parte_3.php
@@ -1,11 +1,8 @@
 <?php
-// comprobar.php - El servidor de La Casa
 
-// 1. Recibimos la ciudad que manda el usuario (si no manda nada, cadena vacía)
 $ciudadRecibida = $_GET['ciudad'] ?? '';
 
-// 2. Base de datos de territorios conquistados (Array de PHP)
-// Puedes añadir las ciudades que quieras aquí
+
 $sedesVIP = [
     "Madrid",
     "Barcelona", 
@@ -18,12 +15,8 @@ $sedesVIP = [
     "Dubai"
 ];
 
-// 3. Lógica de comprobación
-// Usamos 'strtolower' para que no importe si escriben "madrid" o "MaDrId"
-// 'in_array' busca si la ciudad está en la lista
 $existe = false;
 
-// Recorremos el array para comparar ignorando mayúsculas
 foreach ($sedesVIP as $sede) {
     if (strtolower($sede) === strtolower($ciudadRecibida)) {
         $existe = true;
@@ -31,8 +24,6 @@ foreach ($sedesVIP as $sede) {
     }
 }
 
-// 4. Respondemos al JS
-// Solo imprimimos texto plano. Esto es lo que leerá 'xhr.responseText'
 if ($existe) {
     echo "autorizado";
 } else {
